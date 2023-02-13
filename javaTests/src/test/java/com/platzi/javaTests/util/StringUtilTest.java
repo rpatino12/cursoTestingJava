78dcf951e047ef2fd7245b366edabc22b09d00cd
Ricardo Patino <rpatino12@outlook.com>
2023-02-13 17:45:23 -0500

    Created a Unit testing to test the function repeat, without using junit

--- javaTests/src/test/java/com/platzi/javaTests/util/StringUtilTest.java
+++ javaTests/src/test/java/com/platzi/javaTests/util/StringUtilTest.java
@@ -2,14 +2,17 @@ package com.platzi.javaTests.util;
 
 public class StringUtilTest {
     public static void main(String[] args) {
+        // Now we are going to create what is called a Unit testing without using junit
+        // Here we are going to create a function to test the function repeat()
         String result = StringUtil.repeat("Hello", 3);
-        if (!result.equals("HelloHelloHello")){
-            System.out.println("ERROR");
-        }
+        assertEquals(result, "HelloHelloHello");
 
-        String result2 = StringUtil.repeat("Ricardo", 1);
-        if (!result2.equals("Ricardo")){
-            System.out.println("ERROR");
+        assertEquals(StringUtil.repeat("Ricardo", 1), "Ricardo");
+    }
+
+    private static void assertEquals(String actual, String expected) {
+        if (!actual.equals(expected)){
+            throw new RuntimeException(actual + " is not equal to expected: " + expected);
         }
     }
 }
