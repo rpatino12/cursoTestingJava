f30c6e8a60b398b85ffcb0daa7013ce7add8795d
Ricardo Patino <rpatino12@outlook.com>
2023-02-14 15:09:20 -0500

    Added the junit4 library to the pom to create tests easier and create a Unit testing for the repeat method using the class Assert

--- javaTests/src/test/java/com/platzi/javaTests/util/StringUtilTest.java
+++ javaTests/src/test/java/com/platzi/javaTests/util/StringUtilTest.java
@@ -1,18 +1,14 @@
 package com.platzi.javaTests.util;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class StringUtilTest {
-    public static void main(String[] args) {
-        // Now we are going to create what is called a Unit testing without using junit
-        // Here we are going to create a function to test the function repeat()
-        String result = StringUtil.repeat("Hello", 3);
-        assertEquals(result, "HelloHelloHello");
-
-        assertEquals(StringUtil.repeat("Ricardo", 1), "Ricardo");
-    }
-
-    private static void assertEquals(String actual, String expected) {
-        if (!actual.equals(expected)){
-            throw new RuntimeException(actual + " is not equal to expected: " + expected);
-        }
+    @Test
+    public void testRepeat() {
+        // Now using junit4 we can create a Unit testing easily
+        // The method assertEquals of the class Assert for junit has the parameters (expected, actual)
+        assertEquals("HelloHelloHello", StringUtil.repeat("Hello", 3));
+        assertEquals("Ricardo", StringUtil.repeat("Ricardo", 1));
     }
 }
