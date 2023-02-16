8cbc17e39e9abf37a2ba990a711fa922dd58e3ab
Ricardo Patino <rpatino12@outlook.com>
2023-02-16 15:50:25 -0500

    We make a simple example of TDD (Testing driven development), where we create the test to fail, then write the code to pass the test and finally refactor the code

---
+++ javaTests/src/main/java/com/platzi/javaTests/util/DateUtilTDD.java
@@ -0,0 +1,7 @@
+package com.platzi.javaTests.util;
+
+public class DateUtilTDD {
+    public static boolean isLeapYear(int year){
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
---
+++ javaTests/src/test/java/com/platzi/javaTests/util/DateUtilLeapYearShould.java
@@ -0,0 +1,43 @@
+package com.platzi.javaTests.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DateUtilLeapYearShould {
+
+    /*
+        All years divisible by 400 ARE leap years (1600, 2000, 2400)
+        All years divisible by 100 but not by 400 are NOT leap years (1700, 1800, 1900)
+        All years divisible by 4 but not by 100 ARE leap years (1996, 2004, 2008)
+        All years not divisible by 4 are NOT leap years (2017, 2018, 2019)
+    */
+
+    @Test
+    public void return_true_when_year_is_divisible_by_400() {
+        assertTrue(DateUtilTDD.isLeapYear(1600));
+        assertTrue(DateUtilTDD.isLeapYear(2000));
+        assertTrue(DateUtilTDD.isLeapYear(2400));
+    }
+
+    @Test
+    public void return_false_when_year_is_divisible_by_100_but_not_by_400() {
+        assertFalse(DateUtilTDD.isLeapYear(1700));
+        assertFalse(DateUtilTDD.isLeapYear(1800));
+        assertFalse(DateUtilTDD.isLeapYear(1900));
+    }
+
+    @Test
+    public void return_true_when_year_is_divisible_by_4_but_not_by_100() {
+        assertTrue(DateUtilTDD.isLeapYear(1996));
+        assertTrue(DateUtilTDD.isLeapYear(2004));
+        assertTrue(DateUtilTDD.isLeapYear(2008));
+    }
+
+    @Test
+    public void return_false_when_year_is_not_divisible_by_4() {
+        assertFalse(DateUtilTDD.isLeapYear(2017));
+        assertFalse(DateUtilTDD.isLeapYear(2018));
+        assertFalse(DateUtilTDD.isLeapYear(2019));
+    }
+}
