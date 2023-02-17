308aab960307d98b05a81761ad9713bf1038ced8
Ricardo Patino <rpatino12@outlook.com>
2023-02-17 14:19:45 -0500

    We used the TDD to test the method findMoviesByGenre, first create the test to check if returns movies by genre and second design the method to return movies by genre

--- moviesApp/src/main/java/com/platzi/movies/service/MovieService.java
+++ moviesApp/src/main/java/com/platzi/movies/service/MovieService.java
@@ -1,4 +1,28 @@
 package com.platzi.movies.service;
 
+import com.platzi.movies.data.MovieRepository;
+import com.platzi.movies.model.Genre;
+import com.platzi.movies.model.Movie;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 public class MovieService {
+    // We have to connect the service layer to the data layer (repository)
+    MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public Collection<Movie> findMoviesByGenre(Genre genre) {
+
+        // Here we have a few steps:
+            // 1. (.findAll()) Call the movieRepository findAll() method
+            // 2. (.stream().filter()) Filter the results when the movie genre is equal to the argument
+            // 3. (.collect()) Convert the result to a list
+
+        return movieRepository.findAll().stream().
+                filter(movie -> movie.getGenre() == genre).collect(Collectors.toList());
+    }
 }
--- moviesApp/src/test/java/com/platzi/movies/service/MovieServiceTest.java
+++ moviesApp/src/test/java/com/platzi/movies/service/MovieServiceTest.java
@@ -1,7 +1,49 @@
 package com.platzi.movies.service;
 
+import com.platzi.movies.data.MovieRepository;
+import com.platzi.movies.model.Genre;
+import com.platzi.movies.model.Movie;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.Assert.*;
 
 public class MovieServiceTest {
+    @Test
+    public void return_movies_by_genre() {
+        // Here we mock the repository class
+        MovieRepository movieRepository = Mockito.mock(MovieRepository.class);
 
+        // And here mock the behavior that the repository class would have when calling the method findAll()
+        // We mock that the repository is going to return those 7 movies
+        Mockito.when(movieRepository.findAll()).thenReturn(
+                Arrays.asList(
+                        new Movie(1, "Dark Knight", 152, Genre.ACTION),
+                        new Movie(2, "Memento", 113, Genre.THRILLER),
+                        new Movie(3, "There's Something About Mary", 119, Genre.COMEDY),
+                        new Movie(4, "Super 8", 112, Genre.THRILLER),
+                        new Movie(5, "Scream", 111, Genre.HORROR),
+                        new Movie(6, "Home Alone", 103, Genre.COMEDY),
+                        new Movie(7, "Matrix", 136, Genre.ACTION)
+                )
+        );
+
+        // Then we create the movieService instance
+        MovieService movieService = new MovieService(movieRepository);
+
+        // And here we call the movieService method findMoviesByGenre() that we want to test
+        Collection<Movie> movies = movieService.findMoviesByGenre(Genre.COMEDY);
+
+        // To check that findMoviesByGenre() is working and return only the comedy movies
+        // We are going to get the movie's id, so we can check the assertion easily
+        List<Integer> moviesId = movies.stream().map(movie -> movie.getId()).collect(Collectors.toList());
+
+        // Here we check the id, we have to obtain only the comedy movies ids (3 and 6)
+        assertEquals(Arrays.asList(3, 6), moviesId);
+    }
 }
